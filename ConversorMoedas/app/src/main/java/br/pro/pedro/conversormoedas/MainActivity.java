7e5e99706242f1ac9932530ac1fe80729f2265ce
Pedro Maciel <pedromaciel422@gmail.com>
2021-11-18 21:34:08 -0300

    Wip

---
+++ ConversorMoedas/app/src/main/java/br/pro/pedro/conversormoedas/MainActivity.java
@@ -0,0 +1,27 @@
+package br.pro.pedro.conversormoedas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.mViewHolder.editValue = findViewById(R.id.edit_value);
+    }
+
+    private static class ViewHolder {
+        EditText editValue;
+        TextView textDolar;
+        TextView textEuro;
+
+    }
+}
